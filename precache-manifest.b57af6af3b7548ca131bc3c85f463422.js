13e811c262f11b31e10c23bea0e8cbf874c67b6e
sameerul97 <sameerul97@gmail.com>
2021-06-06 12:07:48 +0100

    Updates

---
+++ precache-manifest.b57af6af3b7548ca131bc3c85f463422.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645bcb08e791c6f1bff215796944b8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5276e862ae94673067b8",
+    "url": "/static/css/2.3a0c7bd3.chunk.css"
+  },
+  {
+    "revision": "9b35aad7c9d5f14996ad",
+    "url": "/static/css/main.15f118a8.chunk.css"
+  },
+  {
+    "revision": "5276e862ae94673067b8",
+    "url": "/static/js/2.abb53aac.chunk.js"
+  },
+  {
+    "revision": "7ed25655036eb5e36ee57c27d04702cc",
+    "url": "/static/js/2.abb53aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b35aad7c9d5f14996ad",
+    "url": "/static/js/main.ef29f8d1.chunk.js"
+  },
+  {
+    "revision": "f19fd0acdd1fd7cf0580",
+    "url": "/static/js/runtime-main.d8fd4f8d.js"
+  },
+  {
+    "revision": "abd78dd5e20bb769dcffc60540e73373",
+    "url": "/static/media/profile.abd78dd5.png"
+  },
+  {
+    "revision": "fa8bb6c415e86da53445e9067c526869",
+    "url": "/static/media/sameer_2.fa8bb6c4.glb"
+  }
+]);
