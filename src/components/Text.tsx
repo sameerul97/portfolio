01be6ad5d0c7b267e345db39797812b205379eb7
sameerul <sameerul97@gmail.com>
2021-08-29 20:53:55 +0100

    Add Text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderText = (props: { text: string }) => <h1>{props.text}</h1>
+
+const Text = (props: { text: string }) => {
+  if (props.text) return <HeaderText text={props.text} />
+
+  return <p>No text</p>
+}
+
+export default Text
+
+export { HeaderText }
