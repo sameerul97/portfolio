d108aa890549cccde83b9db9480e5ea3bf2e25a4
sameerul97 <sameerul97@gmail.com>
2022-10-03 13:55:36 +0100

    set max dolly rate of 8

--- src/components/Rig.tsx
+++ src/components/Rig.tsx
@@ -8,12 +8,17 @@ export const Rig = ({ zoom, rate }: { zoom: boolean; rate: number }) => {
 
   useFrame((state, delta) => {
     if (zoom) {
-      rate -= 0.075
+      if (rate >= 8) {
+        rate -= 0.075
+      }
+      console.log(rate)
+
       state.camera.position.lerp(vec.set(0, 7, rate), 0.02)
     } else {
       state.camera.position.lerp(vec.set(-state.mouse.x * 10, 8 + state.mouse.y * 6, 15), 0.02)
     }
   })
-  
+
+  // TODO: Control orbitcontrol props on zoom=true. props include maxPolarAngle={positionCenter}
   return <OrbitControls enablePan={false} enableZoom={false} target={[0, 5, 5]} maxPolarAngle={Math.PI * 0.6} />
 }
