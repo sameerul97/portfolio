954318ba0a836b41e0244ec283ac6e21dbdf9e58
sameerul97 <sameerul97@gmail.com>
2022-10-09 14:55:01 +0100

    Fix orbitcontrol typing error

--- src/components/Rig.tsx
+++ src/components/Rig.tsx
@@ -1,31 +1,26 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, OrbitControlsProps } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import * as THREE from 'three'
 import { useControls } from 'leva'
-
+import { OrbitControls as OB } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls as IOrbitControls } from 'three-stdlib'
 export const Rig = ({ zoom, rate }: { zoom: boolean; rate: number }) => {
   const [vec] = useState(() => new THREE.Vector3())
-  // const { position, azimuth, polar } = useControls({
-  //   position: [0, 7.1, 15],
-  //   azimuth: { value: 0, min: -4, max: Math.PI },
-  //   polar: { value: 1.58, min: 0, max: 9 },
-  // })
-  const controls = useThree((state) => state.controls)
+  const orbitControlsRef = useRef<IOrbitControls>(null)
 
   useFrame((state, delta) => {
     if (zoom) {
       if (rate >= 8) {
         rate -= 0.055
       }
-      console.log(rate)
-      controls?.setAzimuthalAngle(0)
-      controls?.setPolarAngle(1.58)
-      // state.camera.position.lerp(vec.set(0, 7, rate), 0.02)
-      state.camera.position.lerp(vec.set(0, 7.1, rate), 0.02)
+
+      orbitControlsRef.current?.setAzimuthalAngle?.(0)
+      orbitControlsRef.current?.setPolarAngle?.(1.58)
+      state.camera.position.lerp(vec.set(0, 8.5, rate), 0.02)
     } else {
       // state.camera.position.lerp(vec.set(-state.mouse.x * 10, 8 + state.mouse.y * 6, 15), 0.02)
-      state.camera.position.lerp(vec.set(-state.mouse.x * 4, 8 + state.mouse.y * 7.1, 15), 0.02)
+      state.camera.position.lerp(vec.set(-state.mouse.x * 8, 8 + state.mouse.y * 7.1, 15), 0.02)
     }
     // state.camera.position.set(position[0], position[1], position[2])
 
@@ -36,6 +31,14 @@ export const Rig = ({ zoom, rate }: { zoom: boolean; rate: number }) => {
   // TODO: Control orbitcontrol props on zoom=true. props include maxPolarAngle={positionCenter}
   // return <OrbitControls makeDefault enablePan={true} enableZoom={false} />
   return (
-    <OrbitControls makeDefault enablePan={false} enableZoom={false} target={[0, 5, 5]} maxPolarAngle={Math.PI * 0.6} />
+    // <OrbitControls makeDefault enablePan={false} enableZoom={false} target={[0, 5, 5]} maxPolarAngle={Math.PI * 0.6} />
+    <OrbitControls
+      ref={orbitControlsRef}
+      makeDefault
+      enablePan={false}
+      enableZoom={false}
+      target={[0, 5, 5]}
+      maxPolarAngle={Math.PI * 0.6}
+    />
   )
 }
